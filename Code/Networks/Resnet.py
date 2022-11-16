7b59cc358a3839b8261618ae978ab1931de9bfe4
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-16 09:30:24 +0100

    [ADD] Add Resnet network as sister

--- Code/Networks/Resnet.py
+++ Code/Networks/Resnet.py
@@ -11,9 +11,15 @@ def Resnet():
     log = logging.getLogger(__name__)
 
     try:
+
+        # Load the InceptionResNet
         model = keras.applications.InceptionResNetV2(
             include_top=False, weights="imagenet", input_shape=None
         )
         log.info("InceptionResNetV2 loaded successfully")
+
+        # Freeze the model
+        model.trainable = False
+
     except Exception as e:
         log.info(f"InceptionResNetV2 failed to load, reason: {e}")
