a530df98bff05e601bc35bbf88e256fdaf8a365f
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-16 16:06:29 +0100

    [FIX] Fix splitting for Twin case

--- Code/Modules/A_split.py
+++ Code/Modules/A_split.py
@@ -6,10 +6,10 @@ from Code.Classes.Label import Label
 
 def split(args):
 
-    """Split dataset into train, test and validation.
+    """
+    Split dataset into train, test and validation.
 
     Default split for CelebA is used
-
     """
 
     # Read from Excel file containing labels and images names
@@ -25,22 +25,27 @@ def split(args):
     # Split in train, test and validation
     images_train = df.query("Image in @train_set")["Image"]
     labels_train = df.query("Image in @train_set")["Label"]
-    labels_train = list(map(Label, labels_train))
+    labels_train = list(map(Label, images_train, labels_train))
+    images_train = [f"Input/Images/{image}" for image in images_train]
 
     images_validation = df.query("Image in @validation_set")["Image"]
     labels_validation = df.query("Image in @validation_set")["Label"]
-    labels_validation = list(map(Label, labels_validation))
+    labels_validation = list(map(Label, images_validation, labels_validation))
+    images_validation = [f"Input/Images/{image}" for image in images_validation]
 
     images_test = df.query("Image in @test_set")["Image"]
     labels_test = df.query("Image in @test_set")["Label"]
-    labels_test = list(map(Label, labels_test))
+    labels_test = list(map(Label, images_test, labels_test))
+    images_test = [f"Input/Images/{image}" for image in images_test]
 
     # Initialise datasets
     train_set = ImageLoader(
-        images_train, 
-        labels_train, 
-        set_type="train", 
-        batch_size=args.batch
+        images_train,
+        labels_train,
+        set_type="train",
+        batch_size=args.batch,
+        maximum=255,
+        minimum=0,
     )
 
     validation_set = ImageLoader(
@@ -48,8 +53,8 @@ def split(args):
         labels_validation,
         set_type="validation",
         batch_size=args.batch,
-        maximum=train_set.maximum,
-        minimum=train_set.minimum,
+        maximum=255,
+        minimum=0,
     )
 
     test_set = ImageLoader(
@@ -57,8 +62,8 @@ def split(args):
         labels_test,
         set_type="test",
         batch_size=args.batch,
-        maximum=train_set.maximum,
-        minimum=train_set.minimum,
+        maximum=255,
+        minimum=0,
     )
 
     return train_set, validation_set, test_set
