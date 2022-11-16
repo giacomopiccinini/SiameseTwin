8c977a291afdeef6d9c70c895abbc4f893418a79
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-16 09:20:30 +0100

    [ADD] Add Resnet

---
+++ Code/Networks/Resnet.py
@@ -0,0 +1,19 @@
+import logging
+from tensorflow import keras
+
+
+def Resnet():
+
+    """Load the InceptionResNetV2 network, with weights from imagenet"""
+
+    # Set logger
+    logging.basicConfig(level=logging.NOTSET)
+    log = logging.getLogger(__name__)
+
+    try:
+        model = keras.applications.InceptionResNetV2(
+            include_top=False, weights="imagenet", input_shape=None
+        )
+        log.info("InceptionResNetV2 loaded successfully")
+    except Exception as e:
+        log.info(f"InceptionResNetV2 failed to load, reason: {e}")
